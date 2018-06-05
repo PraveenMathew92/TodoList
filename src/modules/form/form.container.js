40ee1b41b49061087f43a84d7754a332d8b629be
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-05 22:19:22 +0530

    added form container

---
+++ src/modules/form/form.container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormContainer extends Component {
+  render() {
+    return (
+      <div>
+        a what whAT
+      </div>
+    );
+  }
+}
+
+export default FormContainer;
