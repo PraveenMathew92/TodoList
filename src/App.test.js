9ea7172761b99420f6919a3c6b15dbcf2dcb96c4
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-08 23:38:50 +0530

    initalized form component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Form from './components/Form';
+import { shallow } from 'enzyme';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component', () => {
+	it('should render form component', () => {
+		const component = shallow(<App />);
+		const childComponent = component.find(Form);
+		expect(childComponent).toHaveLength(1);
+	});
 });
