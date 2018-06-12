9ad19d7ef5d824f6876f492c539c9e75923f25c5
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-13 01:13:57 +0530

    added star rating to todo component

--- src/__tests__/components/Todo.spec.js
+++ src/__tests__/components/Todo.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Todo from './../../components/Todo';
+import StarRating from './../../components/StarRating';
 import { shallow } from 'enzyme';
 
 describe('Todo Component', () =>{
-	it('should display the props passed to it', ()=>{
+	it('should display the text passed to it', ()=>{
 		const component = shallow(<Todo text={'todo text'}/>);
-		expect(component.text()).toEqual('todo text');
+		expect(component.contains('todo text')).toBeTruthy();
+	});
+
+	it('should have a star rating component', ()=>{
+		const component = shallow(<Todo />);
+		expect(component.find(StarRating)).toHaveLength(1);
 	});
 });
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import StarRating from './StarRating';
 
 class Todo extends Component{
 	render(){
 		return(
 			<div>
 				{this.props.text}
+				<StarRating rating={this.props.star} />
 			</div>
 		)
 	}
