778711be9f883e3f4b34c499a20a8242cd303af2
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-13 14:14:09 +0530

    containers for star rating

--- src/containers/StarRating.js
+++ src/containers/StarRating.js
@@ -1,1 +1,15 @@
-StarRating.js
+import { connect } from 'react-redux';
+import StarRating from './../components/StarRating';
+import * as action from './../actions/StarRating';
+
+const mapStateToProps = state => {
+	rating: state.todo[ownprops.index].star
+}
+
+const mapDispatchToProps = dispatch => {
+	updateStar: (starValue) =>{
+		dispatch.actions.updateStar(ownprop.index, starValue)
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StarRating);
