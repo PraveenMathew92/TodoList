197636d7e4f7fb7d5e4d8018b82176b668d7396c
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-18 01:06:23 +0530

    refactored start ratings

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
-const starDisplay = (rating, updateStar) =>{
-	let stars = [ ];
-	for(let i = 0; i < 5; i++){
-		const starValue = i + 1;
-		stars.push(<i className={(starValue <= rating)?"fa fa-star":"fa fa-star-o"}
+const NUMBER_OF_STARS = 5;
+const SOLID_STAR_CLASS = "fa fa-star";
+const EMPTY_STAR_CLASS = "fa fa-star-o";
+const STAR_COLOR = "orange";
+
+const starDisplay = (rating, updateStar) => {
+	let stars = [];
+	for(let starValue = 1; starValue <= NUMBER_OF_STARS; starValue++){
+		stars.push(<i className={
+				(starValue <= rating)? SOLID_STAR_CLASS: EMPTY_STAR_CLASS 
+			}
+			style={{color: STAR_COLOR}}
 			onClick={() => (updateStar(starValue))} />)
 	}
 	return stars;
