6e6d54300d9b9bfd21496ab9529a4b55a6ff0347
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-18 01:40:58 +0530

    extracted update star method

--- src/constants/ActionHandlers.js
+++ src/constants/ActionHandlers.js
@@ -5,14 +5,16 @@ const addTodo = (state, action) => ((state.todo)?
 	{todo: [{text: action.text, star: 0}]}
 );
 
-export const actionHandlers = {
-	[type.ADD_TODO] : addTodo,
-
-	[type.UPDATE_STAR] : (state, action) => {
+const updateStar = (state, action) => {
 		const updatedTodo = {text: state.todo[action.index].text, star: action.star};
 		return {
 		todo: [...state.todo.slice(0, action.index), 
 				updatedTodo, 
 				...state.todo.slice(action.index + 1)]
-	}}
+	}
+}
+
+export const actionHandlers = {
+	[type.ADD_TODO] : addTodo,
+	[type.UPDATE_STAR] : updateStar
 }
