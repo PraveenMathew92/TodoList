909453a5d380832c0474cc3f33a2a73a7f6aad3a
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-17 23:59:47 +0530

    added todolist selectors

--- src/__tests__/selectors/selectors.spec.js
+++ src/__tests__/selectors/selectors.spec.js
@@ -6,4 +6,10 @@ describe('Selectors', () =>{
 		const state = {todo: [{star: 5}, {star: 4}]};
 		expect(fromStore.ratings(state, ownprops)).toEqual(5);
 	});
+
+	it('should select todos in decreasing order of star rating', () =>{
+		const state = {todo: [{star: 4}, {star: 3}, {star: 5}]};
+		const expectedList = [{star: 5}, {star: 4}, {star: 3}];
+		expect(fromStore.todolist(state)).toEqual(expectedList);
+	});
 });
--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -1,3 +1,8 @@
 export const ratings = (state, ownprops) => (
 	state.todo[ownprops.index].star
 )
+
+export const todolist = state =>(
+	state.todo.sort((a,b) => (
+		 b['star']-a['star'] 
+)));
