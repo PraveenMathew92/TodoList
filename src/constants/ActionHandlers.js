47492cbb44fbaae48913c9c7888714d712abc37c
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-18 01:38:53 +0530

    extracted add todo method

--- src/constants/ActionHandlers.js
+++ src/constants/ActionHandlers.js
@@ -1,10 +1,12 @@
 import * as type from './ActionTypes';
 
+const addTodo = (state, action) => ((state.todo)?
+	{todo:[...state.todo, {text: action.text, star: 0}]}:
+	{todo: [{text: action.text, star: 0}]}
+);
+
 export const actionHandlers = {
-	[type.ADD_TODO] : (state, action) => ((state.todo)?
-		{todo:[...state.todo, {text: action.text, star: 0}]}:
-		{todo: [{text: action.text, star: 0}]}
-	),
+	[type.ADD_TODO] : addTodo,
 
 	[type.UPDATE_STAR] : (state, action) => {
 		const updatedTodo = {text: state.todo[action.index].text, star: action.star};
