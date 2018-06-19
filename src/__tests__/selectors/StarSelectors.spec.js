0d0d0999eed156032077e896342972f8211288db
Praveen Mathew <mpraveen@thoughtworks.com>
2018-06-19 21:25:15 +0530

    removed star component test

--- src/__tests__/selectors/StarSelectors.spec.js
+++ src/__tests__/selectors/StarSelectors.spec.js
@@ -6,6 +6,7 @@ const NUMBER_OF_STARS = 5;
 const SOLID_STAR_CLASS = "fa fa-star";
 const EMPTY_STAR_CLASS = "fa fa-star-o";
 const STAR_COLOR = "orange";
+const randomFunction = () => (0);
 
 describe('Star selectors', ()=>{
 	it('should return an array of five', ()=>{
@@ -17,7 +18,7 @@ describe('Star selectors', ()=>{
 	it('should return all empty stars for a zero star rating', ()=>{
 		const ownprops = {index: 0};
 		const state = {todo: [{star: 0}]};
-		const component = mount(<div> { starDisplay(state, ownprops) } </div>);
+		const component = mount(<div> { starDisplay(0, randomFunction) } </div>);
 		component.find('i').forEach((node) =>{
 			expect(node.hasClass(SOLID_STAR_CLASS)).toBeFalsy();
 		})
@@ -26,7 +27,7 @@ describe('Star selectors', ()=>{
 	it('should return all solid stars for a five star rating', ()=>{
 		const ownprops = {index: 0};
 		const state = {todo: [{star: 5}]};
-		const component = mount(<div> { starDisplay(state, ownprops) } </div>);
+		const component = mount(<div> { starDisplay(5, randomFunction) } </div>);
 		component.find('i').forEach((node) =>{
 			expect(node.hasClass(EMPTY_STAR_CLASS)).toBeFalsy();
 		})
